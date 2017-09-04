cbc9e0e911e459381c59a89581754a8b2de79105
Punky <Gunter.Peeters@me.com>
2017-09-04 20:49:00 +0200

    Adding NAV2017 on ISE

--- PSModules/CreateProfileInISE.ps1
+++ PSModules/CreateProfileInISE.ps1
@@ -59,6 +59,18 @@ $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(''Load NAV 2016 CmdLets'',$c
 
 $code =
 {
+ Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1" -WarningAction SilentlyContinue | out-null
+ Import-Module "$env:ProgramFiles\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1" -WarningAction SilentlyContinue | Out-Null
+ Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RoleTailored Client\Microsoft.Dynamics.Nav.Apps.Tools.psd1" -WarningAction SilentlyContinue | Out-Null
+
+ Clear-Host
+ Write-Host ''get-Command -Module ''Microsoft.Dynamics.Nav.*'''' -ForeGroundColor Yellow
+ get-Command -Module ''Microsoft.Dynamics.Nav.*''
+}
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(''Load NAV 2017 CmdLets'',$code,$null)
+
+$code =
+{
   Start-Steroids
 }
 $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(''Start ISE Steroids'',$code,$null)
